65bc78d47fc153d756e90796c40212908032c0ec
DevAlchamist <nayakshubhanshu060705@gmail.com>
2024-02-04 19:19:22 +0530

    responsive

--- app/Home/home.js
+++ app/Home/home.js
@@ -12,35 +12,14 @@ const HomePage = () => {
           <div class="p-16">
             <div class="p-8 bg-white shadow mt-24">
               <div class="grid grid-cols-1 md:grid-cols-3">
-                <div class="grid grid-cols-3 text-center order-last md:order-first mt-20 md:mt-0">
-                  <div>
-                    <p class="font-bold text-gray-700 text-xl">22</p>
-                    <p class="text-gray-400">All Task</p>
-                  </div>
-                  <div>
-                    <p class="font-bold text-gray-700 text-xl">10</p>
-                    <p class="text-gray-400">Daily Avg.</p>
-                  </div>
-                  <div>
-                    <p class="font-bold text-gray-700 text-xl">89</p>
-                    <p class="text-gray-400">Rank</p>
-                  </div>
-                </div>
+                <div class="grid grid-cols-3 text-center md:order-first mt-20 md:mt-0"></div>
                 <div class="relative">
                   <div class="w-52 h-52 bg-indigo-100 border border-gray-300 overflow-hidden mx-auto rounded-full dark:shadow-gray-600 shadow-xl absolute inset-x-0 top-0 -mt-24 flex items-center justify-center text-indigo-500">
                     <Image src={img1} alt="" className=""></Image>
                   </div>
                 </div>
-                <div class="space-x-8 flex justify-between mt-32 md:mt-0 md:justify-center">
-                  <button class="text-white py-2 px-4 uppercase rounded bg-blue-400 hover:bg-blue-500 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
-                    Connect
-                  </button>
-                  <button class="text-white py-2 px-4 uppercase rounded bg-gray-700 hover:bg-gray-800 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
-                    Message
-                  </button>
-                </div>
               </div>
-              <div class="mt-20 text-center pb-12">
+              <div class="mt-[150px] text-center pb-12">
                 <h1 class="text-4xl font-medium text-gray-700">
                   Shubhanshu Nayak,
                   <span class="font-light text-gray-500">18</span>
